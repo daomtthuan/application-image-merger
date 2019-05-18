9d2d2d4c78a3b5f4b4a1fec831500a567c652406
Dao Minh Trung Thuan <dao.mt.thuan@gmail.com>
2019-05-19 02:14:33 +0700

    create ImageMerger

---
+++ ImageMerger/ImageMerger/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+
+namespace ImageMerger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bitmap img = new Bitmap(@"../../../Asset/image.jpg");
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    Color pixel = img.GetPixel(i, j);
+                    Console.WriteLine(pixel);
+                }
+            }
+        }
+    }
+}
